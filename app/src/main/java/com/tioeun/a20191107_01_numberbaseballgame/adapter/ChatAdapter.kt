cd294e6bd4734c98f0ba16eb6a2daf09eb1bfe68
YongMinSun <ddoni1010@gmail.com>
2019-11-07 21:11:51 +0900

    내가 말한건 오른쪽, 컴퓨터가 말한건 왼쪽

--- app/src/main/java/com/tioeun/a20191107_01_numberbaseballgame/adapter/ChatAdapter.kt
+++ app/src/main/java/com/tioeun/a20191107_01_numberbaseballgame/adapter/ChatAdapter.kt
@@ -1,6 +1,7 @@
 package com.tioeun.a20191107_01_numberbaseballgame.adapter
 
 import android.content.Context
+import android.view.Gravity
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -30,6 +31,12 @@ class ChatAdapter(context: Context, res:Int, list:ArrayList<ChatData>) : ArrayAd
 
         var contentTxt = row.findViewById<TextView>(R.id.contentTxt)
 
+        if(data.speaker == "CPU"){
+            contentTxt.gravity = Gravity.LEFT
+        } else {
+            contentTxt.gravity = Gravity.RIGHT
+        }
+        
         contentTxt.text = data.message
 
         return row
